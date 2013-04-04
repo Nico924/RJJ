2067d84cedd36fb2e19563e2d3b7593695c9250e
berniernico <berniernico@hotmail.com>
2013-04-04 12:51:48 +0200

    update

---
+++ NAO_RJJ/build-nao_toolchain/CMakeFiles/NAO_RJJ.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/NAO_RJJ.dir/move.cpp.o"
+  "CMakeFiles/NAO_RJJ.dir/main.cpp.o"
+  "sdk/bin/NAO_RJJ.pdb"
+  "sdk/bin/NAO_RJJ"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/NAO_RJJ.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
